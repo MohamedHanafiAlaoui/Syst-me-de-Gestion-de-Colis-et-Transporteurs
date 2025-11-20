76450a99389f185a949af2e097cfc696fcf0c89b
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-11-20 11:59:21 +0100

    SDGCET-3 Générer un JWT : issuer, subject, role, expiresAt, signature

--- src/main/java/com/example/Transporteurs/dto/AuthRequest.java
+++ src/main/java/com/example/Transporteurs/dto/AuthRequest.java
@@ -1,4 +1,9 @@
 package com.example.Transporteurs.dto;
 
+import lombok.Data;
+
+@Data
 public class AuthRequest {
+    private String username;
+    private String Password;
 }
--- src/main/java/com/example/Transporteurs/service/AuthService.java
+++ src/main/java/com/example/Transporteurs/service/AuthService.java
@@ -1,4 +1,42 @@
 package com.example.Transporteurs.service;
 
+
+import com.example.Transporteurs.dto.AuthRequest;
+import com.example.Transporteurs.dto.AuthResponse;
+import com.example.Transporteurs.model.User;
+import com.example.Transporteurs.repository.UserRepository;
+import com.example.Transporteurs.security.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Service
+@RestController
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder encoder;
+
+    public AuthService authService(AuthRequest request)
+    {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+        if (!user.isActive())
+        {
+            throw new RuntimeException("Compte désactivé");
+        }
+
+        if (!passwordEncode.matches(request.getPassword(),user.getPassword()))
+        {
+            throw new RuntimeException("Password incorrecte");
+        }
+
+        String token=JwtUtil.generateToken(user);
+        return new AuthResponse(token,user.getRole());
+
+    }
+
+
+
 }
