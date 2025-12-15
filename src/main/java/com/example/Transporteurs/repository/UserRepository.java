27df319114dae83a58fee24f9df38a2a06f0d854
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-15 23:32:09 +0100

    Refactor UserRepository: streamline imports and add methods for finding users by username and role

--- src/main/java/com/example/Transporteurs/repository/UserRepository.java
+++ src/main/java/com/example/Transporteurs/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.example.Transporteurs.repository;
 
 import com.example.Transporteurs.model.User;
+import com.example.Transporteurs.model.enm.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends MongoRepository<User,String> {
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Page<User> findByRole(Role role, Pageable pageable);
 
-    Optional<User> findByUsername(String Email);
 }
