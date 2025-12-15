612a9281f02e7e3545b133ca1db90acda360eb46
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-15 23:31:47 +0100

    Refactor User class: streamline imports, fix casing in password field, and update status to TransporteurStatut

--- src/main/java/com/example/Transporteurs/model/User.java
+++ src/main/java/com/example/Transporteurs/model/User.java
@@ -2,32 +2,24 @@ package com.example.Transporteurs.model;
 
 import com.example.Transporteurs.model.enm.Role;
 import com.example.Transporteurs.model.enm.Specialite;
-import com.example.Transporteurs.model.enm.Status;
+import com.example.Transporteurs.model.enm.TransporteurStatut;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-
-@Document(collection = "users")
 @Data
-
+@Document(collection = "users")
 public class User {
-    @org.springframework.data.annotation.Id
+    @Id
     private String id;
     private String username;
-    private String Password;
+    private String password;
     private Role role;
+    private boolean active = true;
 
-    private boolean active = false;
-
-
-
-    //TRANSPORTEUR
-
-    private Status status;
+    private TransporteurStatut statut;
     private Specialite specialite;
+
+    private String email;
+    private String phone;
 }
