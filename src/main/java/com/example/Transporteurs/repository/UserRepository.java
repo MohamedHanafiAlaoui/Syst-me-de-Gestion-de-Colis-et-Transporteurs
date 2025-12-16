2595a16af8630acedf1fdebcdf6dd8651da1633b
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-16 17:40:03 +0100

    Add method to find all users by role in UserRepository

--- src/main/java/com/example/Transporteurs/repository/UserRepository.java
+++ src/main/java/com/example/Transporteurs/repository/UserRepository.java
@@ -5,10 +5,13 @@ import com.example.Transporteurs.model.enm.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
     Page<User> findByRole(Role role, Pageable pageable);
+    List<User> findAllByRole(Role role);
 
 }
